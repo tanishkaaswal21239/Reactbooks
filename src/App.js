f644a04cfa3237f0c31710b45d2cb9e85a54a378
Tanishka <tanishka.aswal2410@gmail.com>
2024-06-05 16:00:55 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import Navbar from './navbar.js';
-import Textform from './textform.js';
-import Textar from './textar.js';
 
 function App() {
   return (
     <>
-    <div className="light-hue-bg"><Navbar/>
-    <div className="container my-3">
-      <Textform/>
-    </div>
-    <Textar/>
+    <div className="light-hue-bg">
+      <Navbar/>
     </div>
     </>
     
--- src/navbar.js
+++ src/navbar.js
@@ -1,30 +1,46 @@
 export default function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary light-hue-bg ">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+  return (
+  <nav class="navbar navbar-expand-lg bg-body-tertiary light-hue-bg ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="bookkie.jpg" width="30" height="24"/>
+    </a>
+  </div>
+</nav></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Features</a>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Pricing</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"/></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
           </li>
-          {/* <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li> */}
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
         </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </nav>
-    );
-  }
-  
-  
+  );
+}
